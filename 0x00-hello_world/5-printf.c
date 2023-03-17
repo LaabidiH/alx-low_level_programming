40fb7a690c7eea29481f0146d363ef5393ebc8a7
Houda LAABIDI <houd.laabidi@gmail.com>
2023-03-16 17:13:20 -0700

    create an line with printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - create line with printf function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("\"Programming is like building a multilingual puzzle");
+return (0);
+}
