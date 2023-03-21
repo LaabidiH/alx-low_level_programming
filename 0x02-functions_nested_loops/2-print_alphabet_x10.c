09e7b176238c2068287abbeb8e8ced73f8ae085b
Houda LAABIDI <houd.laabidi@gmail.com>
2023-03-21 16:14:04 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase 10 time
+ */
+
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+for(j=0;j<10; j++)
+{
+for(i=97;i<123;i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
+}
