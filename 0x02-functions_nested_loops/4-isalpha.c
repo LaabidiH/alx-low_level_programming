98fb9f41da0931d8d95139463527953630be4f5c
Houda LAABIDI <houd.laabidi@gmail.com>
2023-03-21 16:39:26 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - print all alphabet in lowercase 10 time
+ */
+
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
