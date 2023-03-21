45868ee88a58d7106a61ae77580b27bcf18d45e2
Houda LAABIDI <houd.laabidi@gmail.com>
2023-03-21 15:15:25 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Writing a program that prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+long long int c;
+printf("_putchar \n");
+return (0);
+}
