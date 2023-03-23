6d148d0683ba57c8bd1c7bc5ff3d129bd5041301
Houda LAABIDI <houd.laabidi@gmail.com>
2023-03-23 16:36:56 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
 * print_numbers - print numbers 0 - 9
 * return: void
@@ -10,7 +10,7 @@ void print_numbers(void)
 char c;
 for (c = '0'; c <= '9'; c++)
 {
-_putchar(c);
+putchar(c);
 }
-_putchar('\n');
+putchar('\n');
 }
