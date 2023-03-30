a7633d1fa0a217419fb60e8ad22c37e513521966
Houda LAABIDI <houd.laabidi@gmail.com>
2023-03-30 16:11:04 -0700

    udate

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 
-/**
-  * main-  A dream doesn't become reality through magic
-  *
-  * Return: 0
-  */
-
 int main(void)
 {
 int n;
 int a[5];
 int *p;
-
 a[2] = 1024;
 p = &n;
   /*
@@ -22,7 +15,7 @@ p = &n;
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-*(p + 5) = 98;
+  *(p + 5) = 98;
   /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
