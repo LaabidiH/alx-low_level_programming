1f0772e47dc24d66cc8a2ed04c1ecffb58baf4c1
Houda LAABIDI <houd.laabidi@gmail.com>
2023-04-11 17:13:18 -0700

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,12 +2,13 @@
 #include "main.h"
 
 /**
- * count_word - helper function to count the number of words in a string
- * @s: string to evaluate
- *
- * Return: number of words
- */
+* * count_word - helper function to count the number of words in a string
+* * @s: string to evaluate
+* *
+* * Return: number of words
+*/
 int count_word(char *s)
+
 {
 int flag, c, w;
 flag = 0;
@@ -19,20 +20,24 @@ flag = 0;
 else if (flag == 0)
 {
 flag = 1;
+
 w++;
 }
 }
 return (w);
 }
+
 /**
- * **strtow - splits a string into words
- * @str: string to split
- *
- * Return: pointer to an array of strings (Success)
- * or NULL (Error)
- */
+* **strtow - splits a string into words
+* @str: string to split
+*
+* Return: pointer to an array of strings (Success)
+* or NULL (Error)
+*/
+
 char **strtow(char *str)
 {
+
 char **matrix, *tmp;
 int i, k = 0, len = 0, words, c = 0, start, end;
 while (*(str + len))
